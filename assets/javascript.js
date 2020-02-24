ee6ebd4faa3790bf80ab81b401635339d00718dd
Moojig <moojigc@gmail.com>
2020-02-24 17:37:35 -0500

    set up the basic html and css

---
+++ assets/javascript.js
@@ -0,0 +1,20 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+	var password = generatePassword();
+	var passwordText = document.querySelector("#password");
+
+	passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+	var letters = ["a", "b", "c"];
+	var firstCharacter = letters[Math.floor(Math.random()*3)];
+	console.log(firstCharacter);
+}
