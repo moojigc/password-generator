d5d88d979808fdf1bcac135f5d6888a6ae0f2a88
Moojig <moojigc@gmail.com>
2020-02-26 22:17:25 -0500

    Sort of works but doesn't return password of the proper length.

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,6 +1,3 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
 // global variables
 var alphabet = "abcdefghijklmnopqrstuvwxyz";
 var lowercaseLetters = alphabet.split("");
@@ -11,6 +8,7 @@ var generateButton = document.getElementById("generate-button");
 var userCase = false;
 var userNumbers = false;
 var userSpecial = false;
+var passwordLength = undefined;
 
 // Add event listener to generate button
 // generateBtn.addEventListener("click", writePassword);
@@ -36,12 +34,21 @@ function chooseLength() {
 	}
 }
 
+// Clears password length field color when user clicks into field. doesn't work though (; ･`д･´)
+function clearLengthField() {
+	var lengthFieldColor = document.getElementById("password-length").style.backgroundColor;
+	if (lengthFieldColor !== "transparent") {
+		lengthFieldColor = "unset";
+	}
+	console.log(lengthFieldColor); //Even though it doesn't work, console still returns 'transparent'... WHAT
+}
 
 // Adds uppercase 
 function chooseCase() {	
 	var yesUppercase = document.getElementById("yes-uppercase");
 	document.getElementById("case-toggle").style.display = "none";
 	yesUppercase.style.display = "block";
+	userCase = true;
 };
 // Changes choice if user changes mind
 function unChooseCase() {
@@ -65,6 +72,7 @@ function chooseNumbers() {
 	var yesNumbers = document.getElementById("yes-numbers");
 	document.getElementById("numbers-toggle").style.display = "none";
 	yesNumbers.style.display = "block";
+	userNumbers = true;
 };
 // Changes choice if user changes mind
 function unChooseNumbers() {
@@ -87,6 +95,7 @@ function chooseSpecial() {
 	var yesSpecial = document.getElementById("yes-special");
 	document.getElementById("special-toggle").style.display = "none";
 	yesSpecial.style.display = "block";
+	userSpecial = true;
 };
 // Changes choice if user changes mind
 function unChooseSpecial() {
@@ -104,9 +113,48 @@ function unChooseSpecial() {
 	}; 
 };
 
-// Write password to the #password input
+// Returns random number between 0 and 9 when x=1.
+function randomN(x) { 
+	return Math.floor(Math.random(x)*10);
+}
+
+// Write password to the #password textarea
 function writePassword() {
-	var newPassword = document.getElementById("password");
-	newPassword.innerHTML = 
+	var newPasswordField = document.getElementById("password");
+	var specialChar = (",./!@#$%^&*~").split("");
+	var password = []; // Empty array to hold the password before appending it to newPasswordField
+
+	if (passwordLength < 8 || passwordLength > 128 || passwordLength === undefined) {
+		alert("Please enter valid password length and click 'Confirm.'");
+	} else {
+
+		// this loop runs for the number that user entered
+		for (i=0; i <= (passwordLength); i++) {
+			// Randomized non-numerical variables here
+			var randomLetter = alphabet[randomN(1)];
+			var randomLetterUp = alphabet[randomN(1)].toUpperCase();
+			var randomSpecial = specialChar[randomN(1)];
+			var randomCharacter = (randomLetter + randomLetterUp + randomN(1) + randomSpecial);
+			// console.log(randomCharacter);
+			console.log("Random letter " + randomLetter);
+			console.log("Random UP " + randomLetterUp);
+			console.log("Random special " + randomSpecial);
+			console.log("random number " + randomN(1));
+			console.log(randomCharacter);
+			console.log(randomCharacter[randomN(1)]);
+	
+			if (userCase && userNumbers && userSpecial) {
+				console.log(randomCharacter[randomN(1)]);
+				// Adds new random character to password[] array
+				password.push(randomCharacter[randomN(1)]);
+			} else {
+				password.push(randomLetter);
+			}
+		}
+		// end of for loop
+		var newPasswordString = password.join("");
+		newPasswordField.innerHTML = newPasswordString;
+	}
+	
 }
 
